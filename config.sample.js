a87aaaae56ef3b4eda02cdcb96e704af7ea5d349
Marek Kubiczek <marek.kubiczek@imgtec.com>
2017-01-25 07:42:04 +0100

    Update config.js
    Signed-off-by: Marek Kubiczek <marek.kubiczek@imgtec.com>

--- config.sample.js
+++ config.sample.js
@@ -4,10 +4,15 @@ module.exports = {
         access_key : '<access_key>',
         access_secret : '<access_secret>'
     },
+    // influx db configuration
     database : {
         uri : "localhost:8086",
         name : "ew17"
     },
-    color_detector_client_name : "ColorDetector",
+    // name of the client responsible for detecting blocks colours
+    color_detector_client_name: "ColorDetector",
+    // name of the client responsible for controlling conveyor belt
+    conveyor_controller_client_name: "ColorDetector",
+    // your host on which app is running, necessary to set subscription uri
     host : "http://f2c73f4b.ngrok.io"
 };
