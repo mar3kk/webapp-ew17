7a005d06aadac6e39f4770dd60c5e5792e9de959
Hugo Santos <hugosilva_santos@outlook.com>
2017-03-02 15:44:47 +0000

    Added description in config.sample.js

--- config.sample.js
+++ config.sample.js
@@ -1,3 +1,34 @@
+/**
+ * Change the name from: config.sample.js -> config.js. In this file are defined 
+ * the configuration variables for the Embedded Word Demo 2017 - conveyor belt 
+ * with color detection. 
+ * 
+ * -----------------------------------------------------------------------------
+ * The complete aplication uses 3 micro-services orchestraded by Docker Compose:
+ * 
+ * 1- Node.js webapp
+ * 2- Influx DB
+ * 3- Grafana
+ * 
+ * -----------------------------------------------------------------------------
+ * The end user needs to define:
+ * 
+ * 1- url -> Device server instance
+ * 2- API access keys (Dev console):
+ * * 2.1- access_key
+ * * 2.2- access_secret
+ * 3- host- http://IP:3001 address from the machine where this webapp is running. 
+ * This is used to create the subscriptions on the device server. The IP addr 
+ * should be accessible by the device server instance, (Public/Private) use 
+ * Proxy if necessary. 
+ * 
+ * -----------------------------------------------------------------------------
+ * Help:
+ * @Marek_Kubiczek
+ * @Hugo_Santos
+ * 
+ */
+
 module.exports = {
     device_server : {
         url: "https://deviceserver.cretordev.io",
